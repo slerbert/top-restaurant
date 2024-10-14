63a78a9b6a84ca595c31a4790ae565a5f71fa747
Scott <156713928+slerbert@users.noreply.github.com>
2024-10-14 14:00:55 +1100

    Add menu functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import "./assets/css/styles.css";
 import { renderHome } from "./home.js";
-console.log("Hello, world!");
+import { renderMenu } from "./menu.js";
 
 const contentDiv = document.querySelector("#content");
-const homeButtonQ = document.querySelector("#home");
-const menuButtonQ = document.querySelector("#menu");
-const aboutButtonQ = document.querySelector("#about");
-const reservationButtonQ = document.querySelector("#reservation");
+const navQuery = document.querySelector("nav");
 
 const createHtmlElement = (tag, textContent, className, id) => {
     const el = document.createElement(tag);
@@ -18,9 +15,18 @@ const createHtmlElement = (tag, textContent, className, id) => {
     return el;
 };
 
-homeButtonQ.addEventListener("click", () => {
-    contentDiv.replaceChildren();
-    renderHome();
+navQuery.addEventListener("click", (e) => {
+    const id = e.target.id;
+
+    if (id) {
+        contentDiv.replaceChildren();
+        if (id === "home") renderHome();
+        if (id === "menu") renderMenu();
+        // if (id === "about") renderAbout();
+        // if (id === "reservation") renderReservation();
+    }
 });
+
+renderHome();
 
 export { contentDiv, createHtmlElement };
---
+++ src/menu.js
@@ -0,0 +1,47 @@
+import { contentDiv, createHtmlElement } from "./index.js";
+
+class MenuItem {
+    constructor(name, description) {
+        this.name = name;
+        this.description = description;
+    }
+    getHtmlElement() {
+        const item = createHtmlElement("div", "", "menu-item");
+        const title = createHtmlElement("h3", this.name, "title");
+        const desc = createHtmlElement("div", this.description, "description");
+
+        item.appendChild(title);
+        item.appendChild(desc);
+
+        return item;
+    }
+}
+
+export const renderMenu = function() {
+    const menu = createHtmlElement("div", "", "menu");
+
+    const menuElements = [];
+
+    const menuItem1 = new MenuItem(
+        "Heritage Pork Bangers & Silken Yukon Purée",
+        "Exquisite sausages, handcrafted by fifth-generation charcutiers, nestled atop a velvety bed of sustainably, hand-whipped Yukon Gold potatoes. Finished with an opulent drizzle of balsamic-lavender reduction and a whisper of foraged woodland herbs."
+    );
+
+    const menuItem2 = new MenuItem(
+        "Crispy Chicken Schnitzel à la Panache",
+        "An artisanal chicken cutlet, pounded to the precise millimeter for optimum tenderness, encased in a golden crust of Panko breadcrumbs, seasoned with Himalayan volcanic salt and kissed by a rarefied dash of saffron-infused oil. Served alongside a trio of microgreens personally inspected by our chef."
+    );
+
+    const menuItem3 = new MenuItem(
+        "Angus Ribeye with Root Vegetable Mélange",
+        "A symphonic 28-day dry-aged ribeye, sourced from an award-winning Highland cattle farm, flame-seared and adorned with a hand-carved array of seasonal root vegetables, each roasted to peak caramelization. Accompanied by a truffle-oil foam and a whisper of aged Modena balsamic vapors."
+    );
+
+    menuElements.push(menuItem1.getHtmlElement());
+    menuElements.push(menuItem2.getHtmlElement());
+    menuElements.push(menuItem3.getHtmlElement());
+
+    menuElements.forEach(a => menu.appendChild(a));
+    
+    contentDiv.appendChild(menu);
+};
